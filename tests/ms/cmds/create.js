07f604339136ed6831e84e41bfcee0c5a13616d2
Sebastián Alvarez <seba.alvarez16@gmail.com>
2017-01-23 13:00:11 -0300

    Separated create command and reducer

---
+++ tests/ms/cmds/create.js
@@ -0,0 +1,16 @@
+import * as r from './reducers/save'
+
+export function create(msg, done) {
+
+  let seneca = this
+
+  const state = {
+    seneca: seneca,
+    bundle: msg.data,
+  }
+
+  r.save(state)
+  .then(result => done(null, {ok: true, data: result}))
+
+}
+
--- tests/ms/index.js
+++ tests/ms/index.js
@@ -1,5 +1,6 @@
 import * as dbHelpers from '../../lib/db'
 
+import * as cmdCreate from './cmds/create'
 import * as cmdFind from './cmds/find'
 
 export default function (opts) {
@@ -9,23 +10,9 @@ export default function (opts) {
   const print = opts.printFunction
   const pin = opts.pin
 
-  seneca.add(`${pin}, cmd:create`, create)
+  seneca.add(`${pin}, cmd:create`, cmdCreate.create)
+
   seneca.add(`${pin}, cmd:find`, cmdFind.find)
   seneca.add(`${pin}, cmd:find, action: findOr`, cmdFind.findOr)
 
 }
-
-function create(msg, done) {
-
-  let seneca = this
-
-  const state = {
-    seneca: seneca,
-    bundle: msg.data,
-  }
-
-  dbHelpers.savePromisified.call(state.seneca, state.bundle, 'testcollection')
-  .then(result => done(null, {ok: true, data: result}))
-}
-
-
