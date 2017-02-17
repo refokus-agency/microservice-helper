5d93e614e6be536968038efc8ccfdbb0c2500a11
Juan Ignacio Gipponi <beogip@gmail.com>
2017-02-17 11:11:02 -0300

    v3.2.0

---
+++ dist/handler.js
@@ -0,0 +1,29 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.handle = handle;
+
+var _appRootPath = require('app-root-path');
+
+var _appRootPath2 = _interopRequireDefault(_appRootPath);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var errors = require(_appRootPath2.default + '/errors');
+
+function handle(id, err) {
+
+  var key = id.replace(/^#/, "");
+
+  if (errors[key] == undefined) return handle('unknownError', err);
+
+  var e = new Error(errors[key]);
+  e.code = '#' + key;
+  if (err) e.stack = err.stack;
+  if (key === 'unknownError') e.oldError = err;
+
+  return e;
+}
+//# sourceMappingURL=handler.js.map
