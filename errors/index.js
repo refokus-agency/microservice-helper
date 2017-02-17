276094ad62deeff9d25701d7c6d050f82f5403ea
Matías Oveja Smith <matias.ovejasmith@gmail.com>
2017-02-17 11:10:01 -0300

    Issue #29 move ms error handler (#30)
    * issue #29 Move ms-error-handler
    
    * issue #29 Move ms-error-handler
    
    * issue #29 Move ms-error-handler
    
    * requested chages
    
    * Added app-root-path

---
+++ errors/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  /* Test -------------------------------------------------- */
+  unknownError : `Something was unexpected.`,
+  apiFail      : `The SD API fail.`
+}
---
+++ tests/handler.spec.js
@@ -0,0 +1,97 @@
+import {expect} from 'chai'
+import {handle} from '../lib/handler'
+
+describe('Error handler test', function () {
+  it('Given apiFail must return a new error with correct description', function (done) {
+
+    let e = handle('apiFail')
+
+    expect(e)
+      .to
+      .be
+      .an('error')
+    expect(e)
+      .to
+      .have
+      .property('code', '#apiFail')
+    expect(e)
+      .to
+      .have
+      .property('message', 'The SD API fail.')
+
+    done()
+
+  })
+
+  it('Given #apiFail must return a new error with correct description', function (done) {
+
+    let e = handle('#apiFail')
+
+    expect(e)
+      .to
+      .be
+      .an('error')
+    expect(e)
+      .to
+      .have
+      .property('code', '#apiFail')
+    expect(e)
+      .to
+      .have
+      .property('message', 'The SD API fail.')
+
+    done()
+
+  })
+
+  it('Given invalid123 must return Unknown error', function (done) {
+
+    let e = handle('invalid123')
+
+    expect(e)
+      .to
+      .be
+      .an('error')
+    expect(e)
+      .to
+      .have
+      .property('code', '#unknownError')
+    expect(e)
+      .to
+      .have
+      .property('message', 'Something was unexpected.')
+
+    done()
+  })
+
+  it('Given unknownError and a error, must return the old stack and the old error as a property', function (done) {
+
+    const oldError = new Error()
+    const e        = handle('invalid', oldError)
+
+    expect(e)
+      .to
+      .be
+      .an('error')
+    expect(e)
+      .to
+      .have
+      .property('code', '#unknownError')
+    expect(e)
+      .to
+      .have
+      .property('message', 'Something was unexpected.')
+
+    expect(e)
+      .to
+      .have
+      .property('stack', oldError.stack)
+
+    expect(e)
+      .to
+      .have
+      .property('oldError', oldError)
+    done()
+
+  })
+})
