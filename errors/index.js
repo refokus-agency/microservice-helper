887988ac54a8edbc8658060a709c8e284959ef30
Matías Oveja Smith <matias.ovejasmith@gmail.com>
2017-02-22 11:04:19 -0300

    issue #35 handle Joi errors (#36)

--- errors/index.js
+++ errors/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   /* Test -------------------------------------------------- */
-  unknownError : `Something was unexpected.`,
-  apiFail      : `The SD API fail.`
+  unknownError     : `Something was unexpected.`,
+  apiFail          : `The SD API fail.`,
+  schemaValidation : 'One or more params are wrong'
 }
