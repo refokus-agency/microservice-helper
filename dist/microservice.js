ba1cf37b8eb60b38596c58463d185a9c26ef60b2
Juan Ignacio Gipponi <beogip@gmail.com>
2017-06-01 09:50:22 -0300

    v5.0.0

--- dist/microservice.js
+++ dist/microservice.js
@@ -3,114 +3,9 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /**
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      *  Microservices pipeline.
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * @module
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      */
-
-// NOTE: we need to remove no-labels for this project, try to avoid this kind of fixes
-/* eslint-disable no-labels */
-
 exports.doFn = doFn;
 
-var _debug = require('./debug');
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var $pipePromise = function () {
-  /**
-   * Basic Promise object that is transported across de pipeline
-   * @param {Function} fnc - Function to execute in the pipeline
-   * @param {Object} state - Current state of the App
-   * @param {boolean} [critical] - When $critical is present, an exception stop de pipe execution
-   */
-  function $pipePromise(fnc, state, critical) {
-    var _this = this;
-
-    _classCallCheck(this, $pipePromise);
-
-    this.state = state;
-    this.promise = new Promise(function (resolve, reject) {
-      try {
-        var fncCalled = fnc(_this.state);
-
-        if (!(typeof fncCalled.then === 'function')) {
-          return _this._successHandler(fncCalled, resolve);
-        }
-
-        return fncCalled.then(function (fncState) {
-          return _this._successHandler(fncState, resolve);
-        }).catch(function (error) {
-          return _this._errorHandler(error, critical, reject, resolve);
-        });
-      } catch (error) {
-        return _this._errorHandler(error, critical, reject, resolve);
-      }
-    });
-  }
-
-  _createClass($pipePromise, [{
-    key: '_successHandler',
-    value: function _successHandler(nState, resolve) {
-      this.state = Object.assign({}, this.state, nState);
-      return resolve(this.state);
-    }
-  }, {
-    key: '_errorHandler',
-    value: function _errorHandler(error, critical, reject, resolve) {
-      if (critical || this.critical) {
-        this.critical = true;
-        return reject(error);
-      }
-      if (process.env.NODE_ENV === 'production' || process.env.DEBUG) console.warn(error);
-      this.state = Object.assign({}, this.state, { error: error });
-      return resolve(this.state);
-    }
-  }, {
-    key: 'then',
-    value: function then(fnc, critical) {
-      var _this2 = this;
-
-      this.promise = this.promise.then(fnc).then(function (nState) {
-        _this2.state = Object.assign({}, _this2.state, nState);
-
-        return _this2.state;
-      }).catch(function (error) {
-        if (critical || _this2.critical) {
-          _this2.critical = true;
-          throw error;
-        }
-        if (process.env.NODE_ENV === 'production' || process.env.DEBUG) console.warn(error);
-        _this2.state = Object.assign({}, _this2.state, { error: error });
-        return _this2.state;
-      });
-
-      return this;
-    }
-  }, {
-    key: 'debug',
-    value: function debug() {
-      var scope = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'default';
-
-      return this.then((0, _debug.printModule)(scope));
-    }
-  }]);
-
-  return $pipePromise;
-}();
-/**
- *
- * @param {Function} fnc Function to execute in the pipeline.
- * @param {Object} state Current state of the App.
- * @param {boolean} [critical] When $critical is present, an exception stop de pipe execution.
- * @returns {$pipePromise}
- */
-
-
-function $pipe(fnc, state, critical) {
-  return new $pipePromise(fnc, state, critical);
-}
+var _pipePromise = require('./pipePromise');
 
 function _generateState(seneca, msg) {
   var notAllowedKeys = ['caller$', 'meta$', 'plugin$', 'tx$'];
@@ -129,6 +24,13 @@ function _generateState(seneca, msg) {
  * @param {Function} fnc Command function that must be executed.
  * @returns {Object}
  */
+/**
+ *  Microservices pipeline.
+ * @module
+ */
+
+// NOTE: we need to remove no-labels for this project, try to avoid this kind of fixes
+/* eslint-disable no-labels */
 function doFn(fnc) {
   return function (msg, done) {
     var state = _generateState(this, msg);
@@ -136,9 +38,9 @@ function doFn(fnc) {
     var returnData = {};
 
     tryFnc: try {
-      var finalState = fnc($pipe, true, state);
+      var finalState = fnc(_pipePromise.$pipe, true, state);
 
-      if (!(finalState instanceof $pipePromise)) {
+      if (!(finalState instanceof _pipePromise.$pipePromise)) {
         returnData = { ok: true, data: finalState };
         break tryFnc;
       }
---
+++ dist/pipePromise.js
@@ -0,0 +1,157 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.$pipePromise = undefined;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /**
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      *  $pipePromise.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * @module
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      */
+
+
+exports.$pipe = $pipe;
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var $pipePromise = exports.$pipePromise = function () {
+  /**
+   * Basic Promise object that is transported across de pipeline
+   * @param {Function} fnc - Function to execute in the pipeline
+   * @param {Object} state - Current state of the App
+   * @param {boolean} [critical] - When $critical is present, an exception stop de pipe execution
+   */
+  function $pipePromise(fnc, state, critical) {
+    var _this = this;
+
+    _classCallCheck(this, $pipePromise);
+
+    this.state = state;
+    this.promise = new Promise(function (resolve, reject) {
+      try {
+        var fncCalled = fnc(_lodash2.default.cloneDeep(_this.state));
+
+        if (!(typeof fncCalled.then === 'function')) {
+          return _this._successHandler(fncCalled, resolve);
+        }
+
+        return fncCalled.then(function (fncState) {
+          return _this._successHandler(fncState, resolve);
+        }).catch(function (error) {
+          return _this._errorHandler(error, critical, reject, resolve);
+        });
+      } catch (error) {
+        return _this._errorHandler(error, critical, reject, resolve);
+      }
+    });
+  }
+
+  _createClass($pipePromise, [{
+    key: '_successHandler',
+    value: function _successHandler(nState, resolve) {
+      this.state = _lodash2.default.merge(this.state, nState);
+      return resolve(_lodash2.default.cloneDeep(this.state));
+    }
+  }, {
+    key: '_errorHandler',
+    value: function _errorHandler(error, critical, reject, resolve) {
+      if (critical || this.critical) {
+        this.critical = true;
+        return reject(error);
+      }
+
+      if (process.env.NODE_ENV === 'production' || process.env.DEBUG) console.warn(error);
+
+      this.state.error = error;
+      return resolve(_lodash2.default.cloneDeep(this.state));
+    }
+  }, {
+    key: 'then',
+    value: function then(fnc, critical) {
+      var _this2 = this;
+
+      this.promise = this.promise.then(fnc).then(function (nState) {
+        _this2.state = _lodash2.default.merge(_this2.state, nState);
+
+        return _lodash2.default.cloneDeep(_this2.state);
+      }).catch(function (error) {
+        if (critical || _this2.critical) {
+          _this2.critical = true;
+          throw error;
+        }
+
+        if (process.env.NODE_ENV === 'production' || process.env.DEBUG) console.warn(error);
+        _this2.state.error = error;
+        return _lodash2.default.cloneDeep(_this2.state);
+      });
+
+      return this;
+    }
+  }, {
+    key: 'use',
+    value: function use(pipeFnc, critical) {
+      return this.then(function (state) {
+        return pipeFnc($pipe, true, state);
+      }, critical);
+    }
+  }, {
+    key: 'debug',
+    value: function debug() {
+      for (var _len = arguments.length, params = Array(_len), _key = 0; _key < _len; _key++) {
+        params[_key] = arguments[_key];
+      }
+
+      return this.then(function (state) {
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+          for (var _iterator = params[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var param = _step.value;
+
+            console.dir(state[param]);
+          }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+              _iterator.return();
+            }
+          } finally {
+            if (_didIteratorError) {
+              throw _iteratorError;
+            }
+          }
+        }
+
+        return {};
+      });
+    }
+  }]);
+
+  return $pipePromise;
+}();
+
+/**
+ *
+ * @param {Function} fnc Function to execute in the pipeline.
+ * @param {Object} state Current state of the App.
+ * @param {boolean} [critical] When $critical is present, an exception stop de pipe execution.
+ * @returns {$pipePromise}
+ */
+
+
+function $pipe(fnc, state, critical) {
+  return new $pipePromise(fnc, state, critical);
+}
+//# sourceMappingURL=pipePromise.js.map
